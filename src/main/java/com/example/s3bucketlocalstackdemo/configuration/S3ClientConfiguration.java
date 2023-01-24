262dc03c3fb711f5ff0b8fdb71d6de6e73eed6e6
Rafael Moreira <rafaelsmoreiras@gmail.com>
2023-01-24 00:42:01 -0300

    feat: addes Basic AWS Credential configuration

--- src/main/java/com/example/s3bucketlocalstackdemo/configuration/S3ClientConfiguration.java
+++ src/main/java/com/example/s3bucketlocalstackdemo/configuration/S3ClientConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -15,10 +17,22 @@ public class S3ClientConfiguration {
     @Value("${aws.s3.endpoint-url}")
     private String endpointUrl;
     
+    @Value("${aws.s3.access-key-id}")
+    private String accessKeyId;
+    
+    @Value("${aws.s3.secret-key-id}")
+    private String secretKeyId;
+    
     @Bean
     AmazonS3 amazonS3() {
         AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(endpointUrl,
                 Regions.EU_WEST_1.getName());
-        return AmazonS3ClientBuilder.standard().withEndpointConfiguration(endpointConfiguration).withPathStyleAccessEnabled(true).build();
+        
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretKeyId);
+        return AmazonS3ClientBuilder.standard()
+        		.withEndpointConfiguration(endpointConfiguration)
+        		.withCredentials(new AWSStaticCredentialsProvider(credentials))
+        		.withPathStyleAccessEnabled(true)
+        		.build();
     }
 }
